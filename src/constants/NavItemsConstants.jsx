40639e00117633e6bc0b905913ab017667e0be04
@nahid-98 <abdolbaghinahid27@gmail.com>
2023-06-16 23:49:16 +0330

    fix: chenge in styles in speedinformation

--- src/constants/NavItemsConstants.jsx
+++ src/constants/NavItemsConstants.jsx
@@ -7,10 +7,21 @@ export const companyDetails = [
     {icon:ImageContainer.greeTriangle, titel:'مشکلات حل شده و درست', number:'2', progressColor:'#10CCAE' }
  
 ];
+
 export const companyDetails2 = [
     {img:ImageContainer.whiteLockIco, title:'امن'},
     {img:ImageContainer.whiteSourceIcoIco, title:'64 منبع'},
     {img:ImageContainer.whiteScalesOfJusticeIco, title:'38.72 کیلوبایت'},
     {img:ImageContainer.whiteSpeedIco, title:'1.67 ثانیه'}
+    
+];
+
+export const speedInformation = [
+    {img:ImageContainer.yellowSquare, title:'Time to Interactive',time:'3.4 ثانیه'},
+    {img:ImageContainer.redTriangle, title:'First Contentful Paint',time:'12 میلی ثانیه'},
+    {img:ImageContainer.greeTriangle, title:'Total Blocking Time',time:'3.4 ثانیه'},
+    {img:ImageContainer.redTriangle, title:'Speed Index',time:'3.4 ثانیه'},
+    {img:ImageContainer.yellowSquare, title:'Cumulative Layout Shift',time:'3.4 ثانیه'},
+    {img:ImageContainer.redTriangle, title:'Largest Contentful Paint',time:'3.4 ثانیه'}
     
 ];
--- src/pages/BusinessPages/StatisticsAndReportsTab/layout/AllInformation/SpeedInformation.jsx
+++ src/pages/BusinessPages/StatisticsAndReportsTab/layout/AllInformation/SpeedInformation.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { ImageContainer } from '../../../../../assets/img/IMG'
+import { speedInformation } from '../../../../../constants/NavItemsConstants'
 
 export default function index() {
 
@@ -7,27 +8,18 @@ export default function index() {
     <div className=' px-5'>
         <p className='text-title text-sm py-4 text-right'>معیار های سرعت صفحه</p>
         <div className=' grid grid-cols-2 gap-x-20 px-3'>
-            {[0,1,2,3,4,5].map(item=>(
+            {speedInformation.map(item=>(
 
             <div className='flex justify-between items-center py-7 border-y border-border'>
                 <div className='flex justify-center items-center'>
-                    <img src={ImageContainer.yellowSquare} alt="square"/>
-                    <span className='pr-5'>Time to Interactive</span>
+                    <img src={item.img} alt="square"/>
+                    <span className='pr-5'>{item.title}</span>
                 </div>
                 <div className='text-red text-sm'>
-                3.4 ثانیه
+                {item.time}
                 </div>
             </div>
             ))}
-            {/* <div className='flex justify-between items-center py-7 border-y border-border'>
-                <div className='flex justify-center items-center'>
-                    <img src={ImageContainer.yellowSquare} alt="square"/>
-                    <span>Time to Interactive</span>
-                </div>
-                <div className='text-red text-sm'>
-                3.4 ثانیه
-                </div>
-            </div> */}
         </div>
     </div>
   )
